9e8c62588831208305119466a0fca13e8d3b5c89
bax24 <38333245+bax24@users.noreply.github.com>
2022-07-31 19:06:21 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ import torchvision.transforms as transforms
 from torch import optim
 import argparse
 import torch.nn as nn
-from model import NetworkRoi, NetworkFull
+from model import NetworkRoi, NetworkFull, UNETR
 import trainModelCNN as f
 from collections import Counter
 import time
 import monai
+from torch.utils.tensorboard import SummaryWriter
+import torchvision
+from PIL import Image
+
+writer = SummaryWriter("runs/mri")
 
 
 def get_data(train_size, disp=False):
@@ -70,9 +75,36 @@ def get_data(train_size, disp=False):
     return train_db, test_db
 
 
+def get_norm(data):
+    Mri_train = []
+
+    diagnosis = ['AD', 'CN']
+    for diag in diagnosis:
+        for path, dirs, files in os.walk(os.path.join(args.data, diag)):
+            for filename in files:
+                if filename == '.DS_Store':
+                    continue
+                image = os.path.join(args.data, diag, filename)
+                sitk_image = sitk.ReadImage(image)
+
+                # transform into a numpy array
+                MRI = sitk.GetArrayFromImage(sitk_image)
+                Mri_train.append(MRI)
+
+    mri_arr = np.array(Mri_train)
+    mri_tensor = torch.tensor(mri_arr)
+    means = mri_tensor.mean(dim=0, keepdim=True)
+    means = means.squeeze(0)
+    stds = mri_tensor.std(dim=0, keepdim=True)
+    stds = stds.squeeze(0)
+    stds[stds == 0] = 1
+
+    return np.array(means), np.array(stds)
+
+
 class MRIDataset(Dataset):
 
-    def __init__(self, root_dir, transform=None):
+    def __init__(self, means, stds):
         self.__MRI = []
         self.__label = []
 
@@ -85,23 +117,25 @@ class MRIDataset(Dataset):
             self.neg = 'MCI'
             self.pos = 'AD'
 
-        self.root_dir = root_dir
-        self.transform = transform
+        self.root_dir = args.data
+        # self.transform = transform
 
         label_dict = {self.neg: 0, self.pos: 1}
 
         for diag in diagnosis:
-            path = os.path.join(root_dir, diag)
+            path = os.path.join(self.root_dir, diag)
             for _, _, files in os.walk(path):
                 for filename in files:
                     if filename == ".DS_Store":
                         continue
-                    self.__MRI.append(os.path.join(root_dir, diag, filename))
+                    self.__MRI.append(os.path.join(self.root_dir, diag, filename))
                     self.__label.append(label_dict[diag])
 
     def __getitem__(self, index):
         mri_image = sitk.ReadImage(self.__MRI[index])
         mri_image = sitk.GetArrayFromImage(mri_image)
+        # mri_tensor = torch.tensor(mri_image)
+        normalized_mri = (mri_image - means) / stds
         label = self.__label[index]
 
         return mri_image, label
@@ -113,7 +147,7 @@ class MRIDataset(Dataset):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='AD Classifier')
-    parser.add_argument( '--epochs', type=int, default=10, metavar='N',
+    parser.add_argument('--epochs', type=int, default=100, metavar='N',
                         help='number of epochs to train (default: 10)')
     parser.add_argument('--no_cuda', action='store_true', default=False,
                         help='enables CUDA training')
@@ -122,10 +156,10 @@ if __name__ == '__main__':
     parser.add_argument('--data', type=str, default='Batch/', metavar='str',
                         help='folder that contains data (default: S_C)')
     parser.add_argument('--lr', type=float, default=0.001,
-                        help='the learning rate (default: 0.001')
+                        help='the learning rate (default: 0.01')
     parser.add_argument('--kernel', type=int, default=16, metavar='N',
                         help='the kernel size (default: 16')
-    parser.add_argument('--network', type=str, default='V', metavar='str',
+    parser.add_argument('--network', type=str, default='CNN_Full', metavar='str',
                         help='the network name (default: CNN)')
     parser.add_argument('--optim', type=str, default='ADAM', metavar='str',
                         help='the optimizer (default: Adam)')
@@ -155,15 +189,18 @@ if __name__ == '__main__':
     elif args.network == 'CNN_Full':
         model = NetworkFull(init_kernel=args.kernel, device=device)
     elif args.network == 'ViT':
-        model = NetworkB(in_channel=1, out_channel=1, img_size=(128, 128, 96), pos_embed='conv')
+        model = NetworkB(in_channel=1, out_channel=1, img_size=(96, 96, 48), pos_embed='conv')
+        model.classification_head = nn.Sequential(nn.Linear(768, 1))
     elif args.network == 'Chris':
         model = monai.networks.nets.ViT(in_channels=1, patch_size=(16, 16, 16), img_size=(96, 96, 48), pos_embed='conv',
                                         num_classes=2, hidden_size=768, mlp_dim=3072, classification=True, num_heads=12,
                                         dropout_rate=0.0)
-        model.classification_head = nn.Sequential(nn.Linear(768, 2))
-    else:
+        model.classification_head = nn.Sequential(nn.Linear(768, 1))
+    elif args.network == 'test':
         model = monai.networks.nets.ViT(in_channels=1, img_size=(128, 128, 96), pos_embed='conv', classification=True,
-                                        patch_size = (16, 16, 16))
+                                        patch_size=(16, 16, 16))
+    elif args.network == 'unetr':
+        model = UNETR(in_channels=4, out_channels=3, img_size=(128, 128, 96), pos_embed='conv', norm_name='instance')
 
     model.to(device)
 
@@ -173,41 +210,142 @@ if __name__ == '__main__':
 
     # Define my optimizer
     if args.optim == 'ADAM':
-        optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=0.0005)
+        optimizer = optim.Adam(model.parameters(), lr=args.lr)
     else:
         optimizer = optim.RMSprop(model.parameters(), lr=args.lr)
 
     # Define my Loss
-    loss = nn.BCELoss()  # Not used for ViT or CNN for now
+    criterion = nn.BCEWithLogitsLoss()
 
     # Downloading datasets
 
-    dataset = MRIDataset(root_dir=args.data, transform=transforms.ToTensor())
+    means, stds = get_norm(args.data)
+
+    dataset = MRIDataset(means, stds)
 
     train_size = int(0.7 * len(dataset))
     test_size = len(dataset) - train_size
     train_set, test_set = torch.utils.data.random_split(dataset, [train_size, test_size])
+
+    print("Training : ", train_size)
+    print("Testing : ", test_size)
+
+    train_classes = [label for _, label in train_set]
+    print("Train labels distribution :", Counter(train_classes))
+
+    test_classes = [label for _, label in test_set]
+    print("Test labels distribution : ", Counter(test_classes))
 
     # means = train_set.mean(dim=1, keepdim=True)
     # stds = train_set.std(dim=1, keepdim=True)
     # normalized_data = (train_set - means) / stds
 
     train_loader = DataLoader(train_set, batch_size=args.batches, shuffle=True)
-    test_loader = DataLoader(test_set, batch_size=args.batches, shuffle=True)
+    test_loader = DataLoader(test_set, batch_size=test_size, shuffle=True)
 
     train_loss = []
     test_loss = []
+
+    step = 0
 
     for epoch in range(1, args.epochs):
         print("--------------------")
         print("EPOCH " + str(epoch))
         print("--------------------")
-        total_loss = f.train(model, train_loader, optimizer, epoch, loss, device)
-        train_loss.append(total_loss)
-        print("\nTraining ok ! With a loss of ", total_loss)
-        total_loss = f.test(model, test_loader, epoch, loss, device)
-        test_loss.append(total_loss)
-        print("Testing ok ! With a loss of \n", total_loss)
+        # total_loss = f.train(model, train_loader, optimizer, epoch, criterion, device)
+        # Training
+        model.train()
+        losses = []
+        accuracies = []
+        correct = 0
+        for batch_idx, (mri, label) in enumerate(train_loader):
+            # Get data to cuda if possible
+            mri, label = mri.to(device), label.to(device)
+
+            # label = label.unsqueeze(1)
+            data = (mri.type(torch.FloatTensor), label.type(torch.FloatTensor))
+
+            # Forward
+            output = model(data)
+            label = label.to(torch.float32)
+            loss = criterion(output, label)
+            losses.append(loss.item())
+
+            # Backward
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # Calculate running training accuracy
+            pred = (torch.sigmoid(output) > 0.5).float()
+            correct = pred.eq(label.view_as(pred)).sum().item()
+            running_train_acc = float(correct) / float(mri.shape[0])
+
+            # if batch_idx % 10 == 0:
+            # step = (epoch - 1) * args.batches
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * 10, len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader), loss.item()))
+
+            print("Correct = ", correct)
+            writer.add_scalar('training loss/' + id, loss, global_step=step)
+            writer.add_scalar('training accuracy/' + id, running_train_acc, global_step=step)
+            step += 1
+
+        # print("\nTraining ok ! With a loss of ", total_loss)
+        # writer.add_scalar('training loss/'+id, total_loss, global_step=epoch)
+        # writer.add_scalar('training accuracy/' + id, running_train_acc, global_step=step)
+        # total_loss = f.test(model, test_loader, epoch, criterion,  device)
+
+        # Testing
+        # test_loss.append(total_loss)
+        model.eval()
+        test_loss = 0
+        correct = 0
+        TP = 0
+        TN = 0
+        FP = 0
+        FN = 0
+
+        with torch.no_grad():
+            for mri, label in test_loader:
+                mri, label = mri.to(device), label.to(device)
+                # label = label.unsqueeze(1)
+                data = (mri.type(torch.FloatTensor), label.type(torch.FloatTensor))
+                output = model(data)
+
+                label = label.to(torch.float32)
+                test_loss += criterion(output, label).item()
+                pred = (torch.sigmoid(output) > 0.5).float()
+                correct += pred.eq(label.view_as(pred)).sum().item()
+                truths = pred.eq(label.view_as(pred))
+
+                for i, right in enumerate(truths):
+                    if right:
+                        if label[i]:
+                            TP += 1
+                        else:
+                            TN += 1
+                    else:
+                        if label[i]:
+                            FN += 1
+                        else:
+                            FP += 1
+
+        lenght = len(test_loader.dataset)
+        print("length = ", lenght)
+        print("mri0 = ", mri.shape[0])
+        running_test_acc = 100. * correct / len(test_loader.dataset)
+        test_loss_mean = test_loss / len(test_loader.dataset)
+        writer.add_scalar('testing loss/' + id, test_loss_mean, global_step=epoch)
+        writer.add_scalar('testing accuracy/' + id, running_test_acc, global_step=epoch)
+
+        test_loss_mean = test_loss / len(test_loader.dataset)
+        print('\nTest {}: Average loss: {:.6f}, Accuracy: {}/{} ({:.4f}%)'.format(
+            epoch, test_loss_mean, correct, len(test_loader.dataset),
+            100. * correct / len(test_loader.dataset)))
+        print('\nTrue positive :{}, True negative :{}, False positive :{}, False negative :{}'.format(
+            TP, TN, FP, FN))
 
     # Plotting Testing loss
     name = id
@@ -215,8 +353,8 @@ if __name__ == '__main__':
     plt.title('Evolution of Loss curves')
     ax = plt.gca()
     # ax.set_ylim([-30, 30])
-    plt.plot(train_loss, label='Training')
-    plt.plot(test_loss, label='Testing')
+    plt.plot(running_train_acc, label='Training')
+    plt.plot(running_test_acc, label='Testing')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.legend(loc="upper right")
--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from typing import Tuple, Union
-from monai.networks.nets import ViT
+# from monai.networks.nets import ViT
 
 from typing import Sequence, Union
 
@@ -11,6 +11,10 @@ import torch.nn as nn
 
 from monai.networks.blocks.patchembedding import PatchEmbeddingBlock
 from monai.networks.blocks.transformerblock import TransformerBlock
+from monai.networks.blocks import UnetrBasicBlock, UnetrPrUpBlock, UnetrUpBlock
+from monai.networks.blocks.dynunet_block import UnetOutBlock
+# from monai.networks.nets import ViT
+
 
 def conv_block_3d(in_dim, out_dim, stride=1, padding=1, batch_norm=True):
     '''
@@ -82,7 +86,7 @@ def baseline_conv_layers_A(init_kernel=16):
 
 
 class NetworkRoi(nn.Module):
-    def __init__(self, init_kernel, device, n_output=2):
+    def __init__(self, init_kernel, device, n_output=1):
         super(NetworkRoi, self).__init__()
         self.init_kernel = init_kernel
         self.device = device
@@ -98,11 +102,11 @@ class NetworkRoi(nn.Module):
             # nn.Linear(384 * init_kernel, 2304),
             # nn.ReLU(),
             nn.Linear(3 * 3 * 1 * 8 * init_kernel, 512),
-            # nn.ReLU(),
+            nn.ReLU(),
             nn.Dropout(),
             # nn.Linear(2304, 512),
             nn.Linear(512, 10),
-            # nn.ReLU(),
+            nn.ReLU(),
             nn.Dropout(),
             nn.Linear(10, n_output),
             # nn.Sigmoid()
@@ -122,13 +126,13 @@ class NetworkRoi(nn.Module):
         img_feat = img_feat.view(mri.size(0), -1)
         fc_out = self.fc(img_feat)
         # softmax
-        output = F.log_softmax(fc_out)
-        # output = output.squeeze()
+        # output = F.log_softmax(fc_out)
+        output = fc_out.squeeze(1)
         return output
 
 
 class NetworkFull(nn.Module):
-    def __init__(self, init_kernel, device, n_output=2):
+    def __init__(self, init_kernel, device, n_output=1):
         super(NetworkFull, self).__init__()
         self.init_kernel = init_kernel
         self.device = device
@@ -142,13 +146,13 @@ class NetworkFull(nn.Module):
         # kernel * 384 = 12288
         self.fc = nn.Sequential(
             nn.Linear(384 * init_kernel, 2304),
-            # nn.ReLU(),
+            nn.ReLU(),
             nn.Linear(2304, 512),
-            # nn.ReLU(),
+            nn.ReLU(),
             nn.Dropout(),
             # nn.Linear(2304, 512),
             nn.Linear(512, 10),
-            # nn.ReLU(),
+            nn.ReLU(),
             nn.Dropout(),
             nn.Linear(10, n_output),
             # nn.Sigmoid()
@@ -168,8 +172,8 @@ class NetworkFull(nn.Module):
         img_feat = img_feat.view(mri.size(0), -1)
         fc_out = self.fc(img_feat)
         # softmax
-        output = F.log_softmax(fc_out)
-        # output = output.squeeze()
+        # output = F.log_softmax(fc_out)
+        output = fc_out.squeeze(1)
         return output
 
 
@@ -183,11 +187,11 @@ class NetworkB(nn.Module):
                  hidden_size: int = 768,
                  mlp_dim: int = 3072,
                  num_heads: int = 12,
-                 pos_embed: str = "perceptron",
+                 pos_embed: str = "conv",
                  norm_name: Union[Tuple, str] = "instance",
                  conv_block: bool = False,
                  res_block: bool = True,
-                 num_classes: int = 2,
+                 num_classes: int = 1,
                  dropout_rate: float = 0.0,
                  ):
         super().__init__()
@@ -208,17 +212,18 @@ class NetworkB(nn.Module):
             num_classes=num_classes,
             dropout_rate=dropout_rate,
         )
-        self.classification_head = nn.Sequential(nn.Linear(768, 2))
-        #self.layer_out = nn.Softmax()
+        # self.classification_head = nn.Sequential(nn.Linear(768, 1))
+        # self.layer_out = nn.Softmax()
 
     def forward(self, input):
         mri, label = input
         mri = mri.unsqueeze(1)
         x, hidden_states_out = self.vit(mri)
-        output = F.log_softmax(x)
-        #output = self.layer_out(x)
+        # output = F.log_softmax(x)
+        # output = self.layer_out(x)
 
-        return output
+        return x.squeeze(1)
+
 
 class ViT(nn.Module):
     """
@@ -227,20 +232,21 @@ class ViT(nn.Module):
 
     ViT supports Torchscript but only works for Pytorch after 1.8.
     """
+
     def __init__(
-        self,
-        in_channels: int,
-        img_size: Union[Sequence[int], int],
-        patch_size: Union[Sequence[int], int],
-        hidden_size: int = 768,
-        mlp_dim: int = 3072,
-        num_layers: int = 12,
-        num_heads: int = 12,
-        pos_embed: str = "conv",
-        classification: bool = False,
-        num_classes: int = 2,
-        dropout_rate: float = 0.0,
-        spatial_dims: int = 3,
+            self,
+            in_channels: int,
+            img_size: Union[Sequence[int], int],
+            patch_size: Union[Sequence[int], int],
+            hidden_size: int = 768,
+            mlp_dim: int = 3072,
+            num_layers: int = 12,
+            num_heads: int = 12,
+            pos_embed: str = "conv",
+            classification: bool = False,
+            num_classes: int = 2,
+            dropout_rate: float = 0.0,
+            spatial_dims: int = 3,
     ) -> None:
         """
         Args:
@@ -295,8 +301,7 @@ class ViT(nn.Module):
         self.norm = nn.LayerNorm(hidden_size)
         if self.classification:
             self.cls_token = nn.Parameter(torch.zeros(1, 1, hidden_size))
-            self.classification_head = nn.Sequential(nn.Linear(hidden_size, num_classes), nn.Tanh())
-
+            self.classification_head = nn.Sequential(nn.Linear(hidden_size, num_classes))
 
     def forward(self, x):
         x = self.patch_embedding(x)
@@ -311,3 +316,206 @@ class ViT(nn.Module):
         if hasattr(self, "classification_head"):
             x = self.classification_head(x[:, 0])
         return x, hidden_states_out
+
+class UNETR(nn.Module):
+    """
+    UNETR based on: "Hatamizadeh et al.,
+    UNETR: Transformers for 3D Medical Image Segmentation <https://arxiv.org/abs/2103.10504>"
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        img_size: Tuple[int, int, int],
+        feature_size: int = 16,
+        hidden_size: int = 768,
+        mlp_dim: int = 3072,
+        num_heads: int = 12,
+        pos_embed: str = "perceptron",
+        norm_name: Union[Tuple, str] = "instance",
+        conv_block: bool = False,
+        res_block: bool = True,
+        dropout_rate: float = 0.0,
+    ) -> None:
+        """
+        Args:
+            in_channels: dimension of input channels.
+            out_channels: dimension of output channels.
+            img_size: dimension of input image.
+            feature_size: dimension of network feature size.
+            hidden_size: dimension of hidden layer.
+            mlp_dim: dimension of feedforward layer.
+            num_heads: number of attention heads.
+            pos_embed: position embedding layer type.
+            norm_name: feature normalization type and arguments.
+            conv_block: bool argument to determine if convolutional block is used.
+            res_block: bool argument to determine if residual block is used.
+            dropout_rate: faction of the input units to drop.
+        Examples::
+            # for single channel input 4-channel output with patch size of (96,96,96), feature size of 32 and batch norm
+            >>> net = UNETR(in_channels=1, out_channels=4, img_size=(96,96,96), feature_size=32, norm_name='batch')
+            # for 4-channel input 3-channel output with patch size of (128,128,128), conv position embedding and instance norm
+            >>> net = UNETR(in_channels=4, out_channels=3, img_size=(128,128,128), pos_embed='conv', norm_name='instance')
+        """
+
+        super().__init__()
+
+        if not (0 <= dropout_rate <= 1):
+            raise AssertionError("dropout_rate should be between 0 and 1.")
+
+        if hidden_size % num_heads != 0:
+            raise AssertionError("hidden size should be divisible by num_heads.")
+
+        if pos_embed not in ["conv", "perceptron"]:
+            raise KeyError(f"Position embedding layer of type {pos_embed} is not supported.")
+
+        self.num_layers = 12
+        self.patch_size = (16, 16, 16)
+        self.feat_size = (
+            img_size[0] // self.patch_size[0],
+            img_size[1] // self.patch_size[1],
+            img_size[2] // self.patch_size[2],
+        )
+        self.hidden_size = hidden_size
+        self.classification = False
+        self.vit = ViT(
+            in_channels=in_channels,
+            img_size=img_size,
+            patch_size=self.patch_size,
+            hidden_size=hidden_size,
+            mlp_dim=mlp_dim,
+            num_layers=self.num_layers,
+            num_heads=num_heads,
+            pos_embed=pos_embed,
+            classification=self.classification,
+            dropout_rate=dropout_rate,
+        )
+        self.encoder1 = UnetrBasicBlock(
+            spatial_dims=3,
+            in_channels=in_channels,
+            out_channels=feature_size,
+            kernel_size=3,
+            stride=1,
+            norm_name=norm_name,
+            res_block=res_block,
+        )
+        self.encoder2 = UnetrPrUpBlock(
+            spatial_dims=3,
+            in_channels=hidden_size,
+            out_channels=feature_size * 2,
+            num_layer=2,
+            kernel_size=3,
+            stride=1,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            conv_block=conv_block,
+            res_block=res_block,
+        )
+        self.encoder3 = UnetrPrUpBlock(
+            spatial_dims=3,
+            in_channels=hidden_size,
+            out_channels=feature_size * 4,
+            num_layer=1,
+            kernel_size=3,
+            stride=1,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            conv_block=conv_block,
+            res_block=res_block,
+        )
+        self.encoder4 = UnetrPrUpBlock(
+            spatial_dims=3,
+            in_channels=hidden_size,
+            out_channels=feature_size * 8,
+            num_layer=0,
+            kernel_size=3,
+            stride=1,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            conv_block=conv_block,
+            res_block=res_block,
+        )
+        self.decoder5 = UnetrUpBlock(
+            spatial_dims=3,
+            in_channels=hidden_size,
+            out_channels=feature_size * 8,
+            kernel_size=3,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            res_block=res_block,
+        )
+        self.decoder4 = UnetrUpBlock(
+            spatial_dims=3,
+            in_channels=feature_size * 8,
+            out_channels=feature_size * 4,
+            kernel_size=3,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            res_block=res_block,
+        )
+        self.decoder3 = UnetrUpBlock(
+            spatial_dims=3,
+            in_channels=feature_size * 4,
+            out_channels=feature_size * 2,
+            kernel_size=3,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            res_block=res_block,
+        )
+        self.decoder2 = UnetrUpBlock(
+            spatial_dims=3,
+            in_channels=feature_size * 2,
+            out_channels=feature_size,
+            kernel_size=3,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            res_block=res_block,
+        )
+        #self.out = UnetOutBlock(spatial_dims=3, in_channels=feature_size, out_channels=out_channels)  # type: ignore
+        self.out = nn.Linear(8, 1)
+
+    def proj_feat(self, x, hidden_size, feat_size):
+        x = x.view(x.size(0), feat_size[0], feat_size[1], feat_size[2], hidden_size)
+        x = x.permute(0, 4, 1, 2, 3).contiguous()
+        return x
+
+    def load_from(self, weights):
+        with torch.no_grad():
+            res_weight = weights
+            # copy weights from patch embedding
+            for i in weights["state_dict"]:
+                print(i)
+            self.vit.patch_embedding.position_embeddings.copy_(
+                weights["state_dict"]["module.transformer.patch_embedding.position_embeddings_3d"]
+            )
+            self.vit.patch_embedding.cls_token.copy_(
+                weights["state_dict"]["module.transformer.patch_embedding.cls_token"]
+            )
+            self.vit.patch_embedding.patch_embeddings[1].weight.copy_(
+                weights["state_dict"]["module.transformer.patch_embedding.patch_embeddings.1.weight"]
+            )
+            self.vit.patch_embedding.patch_embeddings[1].bias.copy_(
+                weights["state_dict"]["module.transformer.patch_embedding.patch_embeddings.1.bias"]
+            )
+
+            # copy weights from  encoding blocks (default: num of blocks: 12)
+            for bname, block in self.vit.blocks.named_children():
+                print(block)
+                block.loadFrom(weights, n_block=bname)
+            # last norm layer of transformer
+            self.vit.norm.weight.copy_(weights["state_dict"]["module.transformer.norm.weight"])
+            self.vit.norm.bias.copy_(weights["state_dict"]["module.transformer.norm.bias"])
+
+    def forward(self, x_in):
+        x, hidden_states_out = self.vit(x_in)
+        enc1 = self.encoder1(x_in)
+        x2 = hidden_states_out[3]
+        enc2 = self.encoder2(self.proj_feat(x2, self.hidden_size, self.feat_size))
+        x3 = hidden_states_out[6]
+        enc3 = self.encoder3(self.proj_feat(x3, self.hidden_size, self.feat_size))
+        x4 = hidden_states_out[9]
+        enc4 = self.encoder4(self.proj_feat(x4, self.hidden_size, self.feat_size))
+        logits = self.out(enc4)
+        return logits
+
